3711f605e0e224f8e2fb9fe84d497089fff20dad
KarbonST <mikhailbykadorov@mail.ru>
2024-11-13 14:48:06 +0300

    Update create_bot.py

--- scheduleBot/create_bot.py
+++ scheduleBot/create_bot.py
@@ -1,7 +1,9 @@
-import _mysql_connector
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from decouple import config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+import _mysql_connector
+
 
