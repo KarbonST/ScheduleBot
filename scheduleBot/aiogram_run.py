ad8cea3bab211fd752c25e75600103d759506252
KarbonST <mikhailbykadorov@mail.ru>
2024-11-13 23:15:56 +0300

    Update aiogram_run
    Задали основную асинхронную функцию для запуска бота

--- scheduleBot/aiogram_run.py
+++ scheduleBot/aiogram_run.py
@@ -1,2 +1,16 @@
 import asyncio
 from create_bot import bot, dp, scheduler
+from handlers.start import start_router
+
+
+# from work_time.time_func import send_time_msg
+
+# Определение основной асинхронной функции main
+async def main():
+    dp.include_router(start_router)  # Добавление роутера
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)  # Запуск бота в режиме опроса(polling)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- scheduleBot/handlers/start.py
+++ scheduleBot/handlers/start.py
@@ -4,8 +4,11 @@ from aiogram.types import Message
 
 start_router = Router()
 
+
 @start_router.message(CommandStart())
-async def cmd_start(message:Message):
+async def cmd_start(message: Message):
     await message.answer('Привет, я бот расписания ВолгГТУ! Я помогу тебе разобраться'
                          ', где у тебя проходит пара, будь ты студентом или преподавателем!'
                          'Так же ты сможешь узнать расписание любой интересующей тебя аудитории')
+
+
