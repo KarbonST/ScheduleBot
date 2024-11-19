8f8a001b304b5e11dbe7dc752ad2cb764b465a3d
KarbonST <mikhailbykadorov@mail.ru>
2024-11-19 19:55:21 +0300

    Add keyboards
    Добавление основных клавиатур с опциями для взаимодействия с пользователями

--- scheduleBot/handlers/start.py
+++ scheduleBot/handlers/start.py
@@ -1,6 +1,7 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from scheduleBot.keyboards.all_keyboards import main_kb
 
 start_router = Router()
 
@@ -9,6 +10,7 @@ start_router = Router()
 async def cmd_start(message: Message):
     await message.answer('Привет, я бот расписания ВолгГТУ! Я помогу тебе разобраться'
                          ', где у тебя проходит пара, будь ты студентом или преподавателем!'
-                         'Так же ты сможешь узнать расписание любой интересующей тебя аудитории')
+                         'Так же ты сможешь узнать расписание любой интересующей тебя аудитории',
+                         reply_markup=main_kb(message.from_user.id))
 
 
---
+++ scheduleBot/keyboards/all_keyboards.py
@@ -0,0 +1,45 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from scheduleBot.create_bot import admins
+
+
+# Клавиатура "Главное меню"
+def main_kb(user_telegram_id: int):
+    kb_list = [
+        [KeyboardButton(text="📖О нас📖"), KeyboardButton(text="👤Профиль👤")],
+        [KeyboardButton(text="📚Перейти к расписанию📚")]
+    ]
+    if user_telegram_id in admins:
+        kb_list.append([KeyboardButton(text="⚙️Админ панель⚙️")])
+    keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True)
+
+    return keyboard
+
+
+# Клавиатура выбора группы/преподавателя/аудитории
+def schedule_choice_kb(user_telegram_id: int):
+    kb_list = [
+        [KeyboardButton(text="👨‍🎓Группа👩‍🎓")],
+        [KeyboardButton(text="👨‍🏫Преподаватель👩‍🏫"), KeyboardButton(text="🏬Аудитория🏬")],
+        [KeyboardButton(text="⏪Вернуться в главное меню⏪")]
+    ]
+    if user_telegram_id in admins:
+        kb_list.append([KeyboardButton(text="⚙️Админ панель⚙️")])
+    keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True)
+
+    return keyboard
+
+# Клавиатура выбора промежутка расписания(сегодня, завтра, эта неделя, следующая неделя)
+def duration_choice_kb(user_telegram_id: int):
+    kb_list = [
+        [KeyboardButton(text="1️⃣Сегодня1️⃣")],
+        [KeyboardButton(text="2️⃣Завтра2️⃣")],
+        [KeyboardButton(text="3️⃣Эта неделя3️⃣")],
+        [KeyboardButton(text="4️⃣Следующая неделя4️⃣")],
+        [KeyboardButton(text="⏪Вернуться назад⏪"), KeyboardButton(text="⏪Вернуться в главное меню⏪")]
+
+    ]
+    if user_telegram_id in admins:
+        kb_list.append([KeyboardButton(text="⚙️Админ панель⚙️")])
+    keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True)
+
+    return keyboard
